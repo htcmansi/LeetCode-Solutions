a0089209d98eeb20263b6892be1f90145bd2585e
Mansi <85409978+htcmansi@users.noreply.github.com>
2023-07-25 00:15:32 +0530

    Time: 244 ms (52.73%), Space: 19 MB (16.50%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        dict1=collections.defaultdict(list)
+        max_degre=0
+        res=float('inf')
+        for i in range(len(nums)):
+            dict1[nums[i]].append(i+1)
+            max_degre=max(len(dict1[nums[i]]),max_degre)
+        if max_degre==1:
+            return 1
+
+        for i in dict1:
+            if len(dict1[i])==max_degre:
+                res=min(res,dict1[i][-1]-dict1[i][0]+1)
+        return res
+        
