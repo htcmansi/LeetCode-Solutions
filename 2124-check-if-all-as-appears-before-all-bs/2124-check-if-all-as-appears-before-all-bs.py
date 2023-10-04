fb8c045ef5ecf97b6c0568f89f133eeb7ea2256d
Mansi Raut <85409978+htcmansi@users.noreply.github.com>
2023-10-04 22:10:08 +0530

    Time: 37 ms (68.13%), Space: 16.2 MB (74.86%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -1,4 +1,7 @@
 class Solution:
     def checkString(self, s: str) -> bool:
-        return "ba"  not in s
+        for i in range(len(s)-1):
+            if s[i]=='b' and s[i+1]=='a':
+                return False
+        return True
         
