0d842fe830bd5ad927a2e967c2548d601216b8ab
Mansi Raut <85409978+htcmansi@users.noreply.github.com>
2024-03-19 22:08:13 +0530

    Time: 30 ms (92.59%), Space: 16.5 MB (72.43%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -2,6 +2,5 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         decimal_sum=int(a,2)+int(b,2)
         Binary_sum=bin(decimal_sum)
-        binarysum_withoutprefix=(Binary_sum)[2:]
-        return binarysum_withoutprefix
+        return Binary_sum[2:]
         
